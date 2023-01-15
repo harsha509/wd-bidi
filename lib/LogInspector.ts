27a7a0a584c02accb0aa0f18b6ac8d7f5c2ebd22
Sriharsha <sri_harsha509@hotmail.com>
2023-01-15 19:29:42 +0530

    Update log inspector

--- lib/LogInspector.ts
+++ lib/LogInspector.ts
@@ -1,5 +1,3 @@
-onst BIDI = require('./bidi')
-
 const LOG_LEVEL = {
     DEBUG: 'debug',
     ERROR: 'error',
@@ -8,83 +6,90 @@ const LOG_LEVEL = {
 }
 
 class LogInspector {
-    bidi
-    ws
+    bidi: any;
+    ws: any;
 
-    constructor (driver, browsingContextIds) {
-        this.debug = []
-        this.error = []
-        this.info = []
-        this.warn = []
-        this._driver = driver
-        this._browsingContextIds = browsingContextIds
+    debug = []
+    error = []
+    info = []
+    warn = []
+    private _driver: any;
+    private readonly _browsingContextIds: any;
+
+    constructor (driver: any, browsingContextIds: any) {
+        this._driver = driver;
+        this._browsingContextIds = browsingContextIds;
     }
 
-    async init () {
-        this.bidi = await this._driver.getBidi()
+    async init() {
+        this.bidi = await this._driver.getBidi();
         // TODO this.bidi should have bidi instance need to remove connect once api is done
-        await this.bidi.connect()
-        await this.bidi.subscribe('log.entryAdded', this._browsingContextIds)
+        await this.bidi.connect();
+        await this.bidi.subscribe('log.entryAdded', this._browsingContextIds);
     }
 
-    async listen () {
-        this.ws = await this.bidi.socket
+    async listen() {
+        this.ws = await this.bidi.socket;
 
-        this.ws.on('message', event => {
-            const data = JSON.parse(Buffer.from(event.toString()))
-            console.log(data)
+        this.ws.on('message', (event: { toString: () => WithImplicitCoercion<ArrayBuffer | SharedArrayBuffer>; }) => {
+            // @ts-ignore
+            const data = JSON.parse(Buffer.from(event.toString()));
+            console.log(data);
             switch (data.params.level) {
                 case LOG_LEVEL.INFO:
-                    this.info.push(data.params)
-                    break
+                    // @ts-ignore
+                    this.info.push(data.params);
+                    break;
 
                 case LOG_LEVEL.DEBUG:
-                    this.debug.push(data.params)
-                    break
+                    // @ts-ignore
+                    this.debug.push(data.params);
+                    break;
 
                 case LOG_LEVEL.ERROR:
-                    this.error.push(data.params)
-                    break
+                    // @ts-ignore
+                    this.error.push(data.params);
+                    break;
 
                 case LOG_LEVEL.WARNING:
-                    this.warn.push(data.params)
-                    break
+                    // @ts-ignore
+                    this.warn.push(data.params);
+                    break;
 
                 default:
                 // Unknown websocket message type
             }
-        })
+        });
     }
 
-    // TODO below are used to check,will be replaced by spec methods as described in doc
-    get infoLogs () {
-        return this.info
+// TODO below are used to check,will be replaced by spec methods as described in doc
+    get infoLogs() {
+        return this.info;
     }
 
-    get debugLogs () {
-        return this.debug
+    get debugLogs() {
+        return this.debug;
     }
 
-    get errorLogs () {
-        return this.error
+    get errorLogs() {
+        return this.error;
     }
 
-    get warnLogs () {
-        return this.warn
+    get warnLogs() {
+        return this.warn;
     }
-
 }
 
-let instance = undefined
+let instance: any = undefined;
 
-async function getInstance (driver, browsingContextIds) {
+async function getInstance (driver: any, browsingContextIds: any) {
     if (instance === undefined) {
-        instance = new LogInspector(driver, browsingContextIds)
-        await instance.init()
-        await instance.listen()
-        Object.freeze(instance)
+        instance = new LogInspector(driver, browsingContextIds);
+        await instance.init();
+        await instance.listen();
+        Object.freeze(instance);
     }
-    return instance
+    return instance;
 }
 
-module.exports = getInstance
+export default getInstance;
